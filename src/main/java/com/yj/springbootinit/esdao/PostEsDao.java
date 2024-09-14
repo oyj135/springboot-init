9e5227f8eea961dc34600112b61897ace94e7ab1
yangjian <oyjyyds@outlook.com>
2024-09-14 13:44:48 +0800

    springboot-init-后端模版

---
+++ src/main/java/com/yj/springbootinit/esdao/PostEsDao.java
@@ -0,0 +1,14 @@
+package com.yj.springbootinit.esdao;
+
+import com.yj.springbootinit.model.dto.post.PostEsDTO;
+import java.util.List;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * 帖子 ES 操作
+ *
+ */
+public interface PostEsDao extends ElasticsearchRepository<PostEsDTO, Long> {
+
+    List<PostEsDTO> findByUserId(Long userId);
+}
---
+++ src/main/java/com/yj/springbootinit/mapper/PostThumbMapper.java
@@ -0,0 +1,16 @@
+package com.yj.springbootinit.mapper;
+
+import com.yj.springbootinit.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
